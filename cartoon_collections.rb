cbd99620a48e0fad8aabaf29f4fb6d4495f092ca
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-04-08 16:52:24 -0400

    All tests passing

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array_of_names)
+  array_of_names.map.with_index { |name, index| puts "#{index + 1}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array_of_calls)
+  array_of_calls.map { |call| "#{call.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array_of_calls)
+  array_of_calls.any? { |call| call.size > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array_of_strings)
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types = %w[cheddar gouda camembert]
+
+  array_of_strings.find { |cheese| cheese if cheese_types.include? cheese }
 end
